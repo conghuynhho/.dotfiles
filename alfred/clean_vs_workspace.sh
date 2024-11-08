367bae22e5161b2700c8e129e307ad04e8cb8c73
conghuynhho <conghuynhho12@gmail.com>
2024-11-08 17:10:21 +0700

    main update clean_vs_workspace

--- alfred/clean_vs_workspace.sh
+++ alfred/clean_vs_workspace.sh
@@ -12,6 +12,7 @@
 # Define the path to the workspaceStorage directory
 workspace_storage_dir="$HOME/Library/Application Support/Code/User/workspaceStorage"
 workspace_storage_dir_2="$HOME/Library/Application Support/Cursor/User/workspaceStorage"
+workspace_storage_dir_3="$HOME/Library/Application Support/Code - Insiders/User/workspaceStorage"
 
 # Check if the workspaceStorage directory exists
 if [ ! -d "$workspace_storage_dir" ]; then
@@ -59,5 +60,25 @@ while IFS= read -r -d '' subfolder; do
     fi
   fi
 done < <(find "$workspace_storage_dir_2" -mindepth 1 -maxdepth 1 -type d -print0)
+
+while IFS= read -r -d '' subfolder; do
+  # Check if the subfolder contains a workspace.json file
+  if [ -f "$subfolder/workspace.json" ]; then
+    # Extract the project folder path from workspace.json
+    project_folder_url=$(jq -r '.folder' "$subfolder/workspace.json")
+
+    # Remove "file://" prefix and decode URL-encoded characters
+    project_name=$(echo "$project_folder_url" | sed 's|^file://||' | sed 's/%20/ /g')
+
+    # Check if the project folder exists
+    if [ ! -d "$project_name" ]; then
+      echo -e "$RED Project folder '$project_name' not found. Deleting... $NC"
+      rm -rf "$subfolder"
+      echo -e "$RED Deleted: $subfolder $NC"
+    else
+      echo "Project folder '$project_name' found."
+    fi
+  fi
+done < <(find "$workspace_storage_dir_3" -mindepth 1 -maxdepth 1 -type d -print0)
 
 echo "Process complete."
